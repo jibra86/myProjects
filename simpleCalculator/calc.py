6930443c188c406edc2e0388178c215a90a28e83
jibran <jibran@debian>
2022-11-08 17:17:59 +0500

    commit

--- simpleCalculator/calc.py
+++ simpleCalculator/calc.py
@@ -6,6 +6,10 @@ Val1 = input("Value one: ")
 Val2 = input("Value two: ")
 Op = input("Enter the operator: ")
 
+"""
+try-except block to solve the problem that might occur if someone
+un-intentional entered string value instead of numerical value...
+"""
 try:
     fVal1 = float(Val1)
     fVal2 = float(Val2)
@@ -13,11 +17,14 @@ except:
     print("Enter a Numerical Value!!!")
     exit
 
-if Op == "+":
-    print(fVal1 + fVal2)
-elif Op == "-":
-    print(fVal1 - fVal2)
-elif Op == "/":
-    print(fVal1 / fVal2)
-elif Op == "*":
-    print(fVal1 * fVal2)
+"""
+if-elif statements for the calculations....
+"""
+if Op == "+": # if the Op variable is equal to +
+    print(fVal1 + fVal2) # then perform summision
+elif Op == "-": # if the Op variable is equal to -
+    print(fVal1 - fVal2) # then perform subtraction
+elif Op == "/": # if the Op variable is equal to /
+    print(fVal1 / fVal2) # then perform division
+elif Op == "*": # if the Op variable is equal to *
+    print(fVal1 * fVal2) # then perform multiplication
